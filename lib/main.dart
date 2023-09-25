b61d0c3aef40afd8f6aca499a07d8d354a57314e
ZedChen0034 <zed.chen0034@gmail.com>
2023-09-25 17:02:19 +1000

    add calendar.dart as calendar page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:con_nected/calendar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: const Calendar(),
     );
   }
 }
