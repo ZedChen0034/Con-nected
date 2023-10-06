a35eace0d766cf8cb9b1d107bbf06e5bef47cfbb
Nanhao <1966710331@qq.com>
2023-10-07 01:15:07 +1100

    story search

--- lib/story.dart
+++ lib/story.dart
@@ -8,25 +8,74 @@ class Story extends StatefulWidget {
 }
 
 class _StoryState extends State<Story> {
-
   List<GridItem> myItems = GridItem.storyList;
   List<GridItem> likedStories = GridItem.likedStories;
+  bool _isSearching = false;
+  List<GridItem> _filteredItems = [];
 
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _filteredItems = myItems;
+    _searchController.addListener(_onSearchChanged);
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+  _onSearchChanged() {
+    setState(() {
+      String searchText = _searchController.text.toLowerCase();
+      setState(() {
+        if (searchText.isEmpty) {
+          _filteredItems = myItems;
+        } else {
+          _filteredItems = myItems.where((item) =>
+          item.tag!.toLowerCase().contains(searchText) ||
+              item.title.toLowerCase().contains(searchText) ||
+              (item.description?.toLowerCase().contains(searchText) ?? false)
+          ).toList();
+        }
+      });    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title:  Text("Story"),
-        // backgroundColor: Colors.lightGreen[900],
-        leading:
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.search_outlined),
+        title: _isSearching
+            ? TextField(
+          controller: _searchController,
+          autofocus: true,
+          style: const TextStyle(color: Colors.white), // 设置文本颜色为白色
+          decoration: const InputDecoration(
+            hintText: 'Search...',
+            hintStyle: TextStyle(color: Colors.white), // 设置提示文本的颜色为白色
+            border: InputBorder.none, // 去掉下划线
+            focusedBorder: InputBorder.none, // 去掉焦点时的下划线
+          ),
+        )
+            : const Text("Story"),
+        leading: IconButton(
+          onPressed: () {
+            setState(() {
+              if (_isSearching) {
+                _isSearching = false;
+                _searchController.clear();
+              } else {
+                _isSearching = true;
+              }
+            });
+          },
+          icon: _isSearching ? const Icon(Icons.close) : const Icon(Icons.search_outlined),
         ),
       ),
-      body: CustomMasonryGridView(items: myItems,
+      body: CustomMasonryGridView(items: _filteredItems,
         onLikeToggle: (String id) {
           setState(() {
             // Find the item by id
