dd9eff46ea2ac9c386d25a3903d574c940a75a0f
ZedChen0034 <zed.chen0034@gmail.com>
2023-10-07 23:31:36 +1100

    update event list in calendar.dart

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 import 'eventDemo.dart';
@@ -16,15 +17,18 @@ class _CalendarState extends State<Calendar>{
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
-  late final ValueNotifier<List<Event>> _selectedEvents;
-  //List<EventDemo> events = EventDemo.SCRIPT;
+  late final ValueNotifier<List<EventDemo>> _selectedEvents;
+  List<EventDemo> events = EventDemo.SCRIPT;
 
-  Map<DateTime, List<Event>> events = {
-    DateTime.utc(2023,10,2): [const Event("Meeting")],
-    DateTime.utc(2023,10,16): [const Event("Lecture"), const Event("Tutorial")],
-  };
-  List<Event> _getEventsForDay(day){
-    return events[day] ?? [];
+  List<EventDemo> _getEventsForDay(date){
+    Set<EventDemo> results = {};
+    DateFormat dateFormat = DateFormat("yyyy-MM-dd");
+    results.addAll(events.where((user) =>
+        dateFormat.format(date) == dateFormat.format(user.datetime)));
+    var c = results.toList();
+    if (c.isNotEmpty){
+    }
+    return results.toList();
   }
 
   @override
@@ -39,7 +43,7 @@ class _CalendarState extends State<Calendar>{
       appBar: AppBar(
         title: const Text("Event"),
         centerTitle: true,
-        backgroundColor: Colors.lightGreen[900],
+        backgroundColor: Colors.deepPurple[700],
       ),
       body: Column(
         children: [
@@ -71,18 +75,56 @@ class _CalendarState extends State<Calendar>{
           ),
           const SizedBox(height: 8),
           Expanded(
-            child: ValueListenableBuilder<List<Event>>(
+            child: ValueListenableBuilder<List<EventDemo>>(
                 valueListenable: _selectedEvents,
                 builder: (context, value, _){
                   return ListView.builder(itemCount: value.length, itemBuilder: (context, index){
-                    return Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(12),
+                    return Card(
+                      key: ValueKey(value[index].id),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
                       ),
-                      child: ListTile(
-                        title: Text('${value[index]}'),
+                      color: Colors.transparent,
+                      elevation: 8,
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 10),
+                      child: Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [Colors.purple, Colors.blue],
+                              // gradient colors
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          child: Padding(padding:const EdgeInsets.symmetric(vertical: 10.0) ,child: ListTile(
+                            leading: const Icon(
+                              Icons.event,
+                              color: Colors.white,
+                              size: 35,
+                            ),
+                            title: Text(
+                              DateFormat('yyyy-MM-dd HH:mm')
+                                  .format(value[index].datetime) +
+                                  '\n' +
+                                  value[index].name +
+                                  " - " +
+                                  value[index].tag,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            subtitle: Text(
+                              value[index].location,
+                              style: TextStyle(color: Colors.white60),
+                            ),
+                            onTap: () {
+                              Navigator.pushNamed(
+                                context,
+                                '/detail',
+                                arguments: value[index],
+                              );
+                            },
+                          ),)
                       ),
                     );
                   });
