7b80b12c09a7791c5afa83803cf4a4e3b8946080
Nanhao <1966710331@qq.com>
2023-10-08 18:33:37 +1100

    notification rightslide delete

--- lib/Notifications.dart
+++ lib/Notifications.dart
@@ -9,9 +9,18 @@ class Notifications extends StatefulWidget {
 
 class _NotificationsState extends State<Notifications> {
   List<NotificationItem> notifications = [
-    NotificationItem(sender: 'System', content: 'xxx has successfully reached the third phase.', timestamp: DateTime.now()),
-    NotificationItem(sender: 'Warning', content: 'Note: Your penalty points are too high!', timestamp: DateTime.now().subtract(Duration(hours: 1))),
-    // ... add more sample messages
+    NotificationItem(
+        sender: 'System',
+        content: 'xxx has successfully reached the third phase.',
+        timestamp: DateTime.now()),
+    NotificationItem(
+        sender: 'Warning',
+        content: 'Note: Your penalty points are too high!',
+        timestamp: DateTime.now().subtract(Duration(hours: 1))),
+    NotificationItem(
+        sender: 'System',
+        content: 'xxx has successfully reached the second phase.',
+        timestamp: DateTime.now().subtract(Duration(days: 1))),
   ];
 
   @override
@@ -23,15 +32,41 @@ class _NotificationsState extends State<Notifications> {
       body: ListView.builder(
         itemCount: notifications.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            leading: CircleAvatar(
-              child: Text(notifications[index].sender[0]),
-              backgroundColor: notifications[index].sender == 'Warning' ? Colors.red : Colors.blue,
+          return Dismissible(
+            key: Key(notifications[index].timestamp.toString()),
+            // Unique key for Dismissible
+            direction: DismissDirection.endToStart,
+            background: Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              color: Colors.red,
+              child: Icon(Icons.delete, color: Colors.white),
             ),
-            title: Text(notifications[index].content),
-            subtitle: Text(DateFormat('yyyy-MM-dd HH:mm')
-                .format(notifications[index].timestamp)),
-            trailing: Icon(Icons.arrow_forward_ios, size: 14.0, color: Colors.grey[400]),
+            onDismissed: (direction) {
+              setState(() {
+                notifications.removeAt(index);
+              });
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Notification dismissed'),
+                  duration: Duration(
+                      milliseconds: 500),
+                ),
+              );
+            },
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Text(notifications[index].sender[0]),
+                backgroundColor: notifications[index].sender == 'Warning'
+                    ? Colors.red
+                    : Colors.blue,
+              ),
+              title: Text(notifications[index].content),
+              subtitle: Text(DateFormat('yyyy-MM-dd HH:mm')
+                  .format(notifications[index].timestamp)),
+              trailing: Icon(Icons.arrow_forward_ios,
+                  size: 14.0, color: Colors.grey[400]),
+            ),
           );
         },
       ),
